c5890217628fcff1189bfa84fc09da3663aad5e1
Cristian Guerra <cristian@digithaigroup.com>
2021-02-01 12:18:43 +0700

    Add Star Admin template
    https://github.com/app-generator/jinja-star-admin

--- app/views/auth.py
+++ app/views/auth.py
@@ -14,9 +14,9 @@ def login():
     return render_template('login.html')
 
 
-@auth.route('/signup')
+@auth.route('/register')
 def signup():
-    return render_template('signup.html')
+    return render_template('register.html')
 
 
 @auth.route('/logout')
